884d4eabdd9acca3acb6ba239ce82ac0bcc7e320
Unknown <m.schloegl@gmail.com>
2018-05-03 20:07:57 +0200

    register models in admin

--- twiscrape/admin.py
+++ twiscrape/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Account, Message, Project
 
 # Register your models here.
+admin.site.register(Account)
+admin.site.register(Message)
+admin.site.register(Project)
